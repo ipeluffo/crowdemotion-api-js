0a4c7ec6b50049a09d2bacd0e5b7452d1f14ebe6
stecrv <stefano.caravana.service@gmail.com>
2014-07-22 12:39:12 +0200

    Dashboard #BE-94

--- tests/js/drawGraph.js
+++ tests/js/drawGraph.js
@@ -1,6 +1,69 @@
 var line1_rest;
 var emo_labels = ["happy", "surprised", "puzzled", "disgusted", "afraid", "sad"];
 
+(function (H) {
+    var addEvent = H.addEvent;
+
+    H.wrap(H.Chart.prototype, 'init', function (proceed) {
+        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+
+        var chart = this,
+            legend = chart.legend,
+            title = legend.title,
+            options = legend.options,
+            isDragging,
+            downX,
+            downY,
+            optionsX,
+            optionsY,
+            currentX,
+            currentY;
+
+
+        if (options.draggable && title) {
+
+            title.css({ cursor: 'move' });
+
+            addEvent(title.element, 'mousedown', function (e) {
+                e = chart.pointer.normalize(e);
+                downX = e.chartX;
+                downY = e.chartY;
+                optionsX = options.x;
+                optionsY = options.y;
+                currentX = legend.group.attr('translateX');
+                currentY = legend.group.attr('translateY');
+                isDragging = true;
+            });
+            addEvent(chart.container, 'mousemove', function (e) {
+                if (isDragging) {
+                    e = chart.pointer.normalize(e);
+                    var draggedX = e.chartX - downX,
+                        draggedY = e.chartY - downY;
+
+                    options.x = optionsX + draggedX;
+                    options.y = optionsY + draggedY;
+
+                    // Do the move is we're inside the chart
+                    if (currentX + draggedX > 0 &&
+                        currentX + draggedX + legend.legendWidth < chart.chartWidth &&
+                        currentY + draggedY > 0 &&
+                        currentY + draggedY + legend.legendHeight < chart.chartHeight           ) {
+                        legend.group.placed = false; // prevent animation
+                        legend.group.align(H.extend({
+                            width: legend.legendWidth,
+                            height: legend.legendHeight
+                        }, options), true, 'spacingBox');
+                    }
+
+                }
+            });
+            addEvent(document, 'mouseup', function () {
+                isDragging = false;
+            });
+        }
+    });
+}(Highcharts));
+// End plugin
 
 function draw_graphs_rest(data)
 {
@@ -8,7 +71,6 @@ function draw_graphs_rest(data)
     elName = 'graph';
     if(data==undefined){ data = []; }
 
-    console.log(data);
     var xlabels = [];
 
     if(!line1_rest || (line1_rest.series.length<= 0)) {
@@ -19,7 +81,13 @@ function draw_graphs_rest(data)
         for(var i=0; i < data.length; i++) {
             series_rest.push({
                 name: emo_labels[i],
-                data: data[i]
+                data: data[i],
+                lineWidth: 1,
+                marker: {
+                    enabled: true,
+                    symbol: null,
+                    radius: 3
+                }
             });
         }
 
@@ -43,7 +111,7 @@ function draw_graphs_rest(data)
             },
             xAxis: {
                 min: 0,
-                categories: xlabels,
+                categories: null,
                 labels: {
                     rotation: -35,
                     align: 'right'
@@ -62,17 +130,25 @@ function draw_graphs_rest(data)
                     width: 1,
                     color: '#808080'
                 }]
-            }, /*
+            },
             tooltip: {
                 formatter: tooltipFormatter
-            }, */
+            },
             legend: {
                 layout: 'vertical',
                 align: 'right',
                 verticalAlign: 'top',
                 x: -10,
                 y: 60,
-                borderWidth: 0
+                backgroundColor: 'white',
+                borderWidth: 1,
+                borderRadius: 0,
+                title: {
+                    text: ':: Drag Legend'
+                },
+                floating: true,
+                draggable: true,
+                zIndex: 20
             },
             series: series_rest
         });
@@ -106,5 +182,5 @@ function showGraph(data)
 }
 
 function tooltipFormatter() {
-    return '<b>'+ this.series.name +'</b><br/>'+ (this.x!=null?this.x:'value') +': '+ Math.round(this.y*100)/100;
+    return '<b>'+ this.series.name +'</b><br/>'+ ((this.x!=null)?(Math.round(this.y*10)/10):'value') +': '+ Math.round(this.y*100)/100;
 }
